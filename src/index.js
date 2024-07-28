b7a2d3c40cd7d89cc85a8afef402e9586cf2557c
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2024-07-27 21:16:43 -0400

    Add material icons and add icons to src html

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import { Project } from "./helpers";
 import { Task } from "./helpers";
 import { ElementCreator } from "./DOMLoader";
 import { ElementAppender } from "./DOMLoader";
+import './styles.css';
+import 'material-icons/iconfont/material-icons.css';
+
+

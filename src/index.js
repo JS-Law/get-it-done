4e8034c6be9f6f3a6073801d9b0fd7400d1b8c1d
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2024-08-05 22:14:34 -0400

    update styling, add form pop out

--- src/index.js
+++ src/index.js
@@ -6,74 +6,98 @@ import { ElementAppender } from "./DOMLoader";
 import './styles.css';
 import 'material-icons/iconfont/material-icons.css';
 import { getTimestamp } from "./helpers";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const contentContainer = document.querySelector('#main-content');
     contentContainer.style.display = 'grid';
-    contentContainer.style.width = '100vw'
-    contentContainer.style.height = '100vh'
-    contentContainer.style.gridTemplateColumns = '200px 1fr'
-    contentContainer.style.gridTemplateRows = '1fr'
+    contentContainer.style.width = '100vw';
+    contentContainer.style.height = '100vh';
+    contentContainer.style.gridTemplateColumns = '200px 1fr';
+    contentContainer.style.gridTemplateRows = '1fr';
 
-
-    let sideBar = document.querySelector('#side-bar')
-    sideBar.style.display = 'grid'
-    sideBar.style.gridTemplateRows = 'repeat(12, 1fr)'
-    sideBar.style.gridTemplateColumns = '1fr'
+    let sideBar = document.querySelector('#side-bar');
+    sideBar.style.display = 'grid';
+    sideBar.style.gridTemplateRows = 'repeat(12, 1fr)';
+    sideBar.style.gridTemplateColumns = '1fr';
     
+    let projectSection = document.querySelector('#project-section');
+    projectSection.style.display = 'flex';
     
-    let projectSection = document.querySelector('#project-section')
-    projectSection.style.display = 'flex'
+    new ElementAppender(contentContainer, sideBar, projectSection).appendElements();
 
-    
+    const addProjectButton = document.getElementById('add-project-button');
+    const projectFormContainer = document.getElementById('project-form-container');
+    const projectForm = document.getElementById('project-form');
+    const projectDueDateInput = document.getElementById('project-due-date');
 
-    new ElementAppender(contentContainer, sideBar, projectSection).appendElements()
+    flatpickr(projectDueDateInput, {
+        dateFormat: 'M-d-Y',
+    });
 
+    addProjectButton.addEventListener('click', () => {
+        projectFormContainer.classList.toggle('hidden');
+    });
 
-})
+    projectForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-let testProject = new Project('Project 1', '2023-12-31');
-let testProjectTwo = new Project('Project 1', '2023-12-31');
+        const projectName = document.getElementById('project-name').value;
+        const dueDate = projectDueDateInput.value;
 
-let testTask = new Task(
-    'Finish the thing 1',
-    getTimestamp(),
-    getTimestamp(),
-    'High',
-    'Go to the place to do the thing, NOW',
-    'Remember to do that thing',
-    [
-        'Go to place',
-        'Do that thing', 
-    ],
-    'Not Started'
-);
-
-let testTaskTwo = new Task(
-    'Finish the thing 2',
-    getTimestamp(),
-    getTimestamp(),
-    'High',
-    'Go to the place to do the thing, NOW',
-    'Remember to do that thing',
-    [
-        'Go to place',
-        'Do that thing', 
-    ],
-    'Not Started'
-);
-
-testProject.addNewTask(testTask);
-testProject.addNewTask(testTaskTwo);
+        if (projectName && dueDate) {
+            const newProject = new Project(projectName, dueDate);
+            displayTasks(newProject);
+            projectForm.reset();
+            projectFormContainer.classList.add('hidden');
+        }
+    });
+});
 
 
-testProjectTwo.addNewTask(testTaskTwo)
-testProjectTwo.addNewTask(testTask)
+// let testProject = new Project('Project 1', '2023-12-31');
+// let testProjectTwo = new Project('Project 1', '2023-12-31');
+
+// let testTask = new Task(
+//     'Finish the thing 1',
+//     getTimestamp(),
+//     getTimestamp(),
+//     'High',
+//     'Go to the place to do the thing, NOW',
+//     'Remember to do that thing',
+//     [
+//         'Go to place',
+//         'Do that thing', 
+//     ],
+//     'Not Started'
+// );
+
+// let testTaskTwo = new Task(
+//     'Finish the thing 2',
+//     getTimestamp(),
+//     getTimestamp(),
+//     'High',
+//     'Go to the place to do the thing, NOW',
+//     'Remember to do that thing',
+//     [
+//         'Go to place',
+//         'Do that thing', 
+//     ],
+//     'Not Started'
+// );
+
+// testProject.addNewTask(testTask);
+// testProject.addNewTask(testTaskTwo);
 
 
-testTaskTwo.addItemToCheckList('something', 'else');
-displayTasks(testProjectTwo)
-displayTasks(testProject)
-displayTasks(testProject)
-displayTasks(testProject)
+// testProjectTwo.addNewTask(testTaskTwo)
+// testProjectTwo.addNewTask(testTask)
+
+
+// testTaskTwo.addItemToCheckList('something', 'else');
+// displayTasks(testProjectTwo)
+// displayTasks(testProject)
+// displayTasks(testProject)
+// displayTasks(testProject)
